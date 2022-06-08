13742f46ce519303a95a1548252e13219808db86
TheMultii <ganczarczyk.marcel@gmail.com>
2022-06-08 10:58:21 +0200

    OrderIDToAddOrEditConverter

---
+++ Converters/OrderIDToAddOrEditConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace BookStoreP4.Converters {
+    public class OrderIDToAddOrEditConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            if (value is int) {
+                return (int)value != 0 ? "Edytuj zamówienie" : "Dodaj zamówienie";
+            }
+            return "Dodaj zamówienie";
+        }
+        
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
+}
